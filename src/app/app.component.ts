0a6d7d1853c23282d0fe686caf0a29ef917ed7e4
At1X <amirmahdijafarian@gmail.com>
2023-12-24 19:35:18 +0330

    add image slider and icon configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCircleDot, faCircleArrowRight, faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'internship-winter-mohaymen';
+  faCoffee = faCircleDot
+  faArrowLeft = faCircleArrowLeft
+  faArrowRight = faCircleArrowRight
 }
