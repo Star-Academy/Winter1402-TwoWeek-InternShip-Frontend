5f73d7564baf3f32e36023e06f6f4896575e0e34
At1X <amirmahdijafarian@gmail.com>
2024-01-09 17:43:56 +0330

    create a function for calculate updateDate

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -8,16 +8,20 @@ import { NewsData } from "../../models/types";
 })
 export class NewsComponent implements OnInit {
   public news: NewsData[] = [
-    {id: 1 ,title: "Website Maintainace", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident sequi temporibus. Atque dolor est fuga labore laborum quisquam reiciendis repellat rerum sit sunt. Fuga iste iure nobis quasi sequi.", updatedDate: new Date((new Date()).getTime() - 24*60*60*1000), showMore: "#"},
-    {id: 2 ,title: "Website Maintainace", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident sequi temporibus. Atque dolor est fuga labore laborum quisquam reiciendis repellat rerum sit sunt. Fuga iste iure nobis quasi sequi.", updatedDate: new Date((new Date()).getTime() - 24*60*60*50), showMore: "#"},
-    {id: 3 ,title: "Website Maintainace", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident sequi temporibus. Atque dolor est fuga labore laborum quisquam reiciendis repellat rerum sit sunt. Fuga iste iure nobis quasi sequi.", updatedDate: new Date((new Date()).getTime() - 24*60*60*200), showMore: "#"},
-    {id: 4 ,title: "Website Maintainace", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident sequi temporibus. Atque dolor est fuga labore laborum quisquam reiciendis repellat rerum sit sunt. Fuga iste iure nobis quasi sequi.", updatedDate: new Date((new Date()).getTime() - 24*60*60*5), showMore: "#"},
+    {id: 1 ,title: "Website Maintainace", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident sequi temporibus. Atque dolor est fuga labore laborum quisquam reiciendis repellat rerum sit sunt. Fuga iste iure nobis quasi sequi.", updatedDate: this.calculateDate(1000), showMore: "#"},
+    {id: 2 ,title: "Website Maintainace", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident sequi temporibus. Atque dolor est fuga labore laborum quisquam reiciendis repellat rerum sit sunt. Fuga iste iure nobis quasi sequi.", updatedDate: this.calculateDate(50), showMore: "#"},
+    {id: 3 ,title: "Website Maintainace", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident sequi temporibus. Atque dolor est fuga labore laborum quisquam reiciendis repellat rerum sit sunt. Fuga iste iure nobis quasi sequi.", updatedDate: this.calculateDate(200), showMore: "#"},
+    {id: 4 ,title: "Website Maintainace", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident sequi temporibus. Atque dolor est fuga labore laborum quisquam reiciendis repellat rerum sit sunt. Fuga iste iure nobis quasi sequi.", updatedDate: this.calculateDate(5), showMore: "#"},
   ]
   public selectedNews: NewsData[] = [];
 
   ngOnInit(): void {
     this.news.sort((a, b) => b.updatedDate.getTime() - a.updatedDate.getTime());
     this.selectedNews = this.news.slice(0, 2);
+  }
+
+  private calculateDate(sec: number) : Date {
+    return new Date(new Date().getTime() - 24*60*60*sec)
   }
 
 }
