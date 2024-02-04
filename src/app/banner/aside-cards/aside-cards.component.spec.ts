11164f43873fe54929ccec5f65174cc2aba903da
At1X <a.khodaei@ec.iut.ac.ir>
2024-02-04 17:43:55 +0330

    add test for aside card template and functions

--- src/app/banner/aside-cards/aside-cards.component.spec.ts
+++ src/app/banner/aside-cards/aside-cards.component.spec.ts
@@ -1,10 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AsideCardsComponent } from './aside-cards.component';
+import {AsideCard} from "../../../models/types";
+import {By} from "@angular/platform-browser";
 
-describe('AsideCardsComponent', () => {
+fdescribe('AsideCardsComponent', () => {
+  const MOCKED_DATA: AsideCard = {
+    bannerImage: "FAKED_IMAGE",
+    title: "FAKED_TITLE",
+    asideImageURL: "FAKED_ASIDE_URL",
+    id: 952
+  }
+
   let component: AsideCardsComponent;
   let fixture: ComponentFixture<AsideCardsComponent>;
+  let host: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,11 +22,27 @@ describe('AsideCardsComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(AsideCardsComponent);
+    host = fixture.nativeElement;
     component = fixture.componentInstance;
+    component.data = MOCKED_DATA;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('[Template] title SHOULD have exact data WHEN initialize', () => {
+    // Arrange
+    const elem: HTMLElement = fixture.debugElement.query(By.css("[dataTestID=\"aside-title\"]")).nativeElement
+
+    // Assert
+    expect(elem.innerText).toBe(MOCKED_DATA.title)
+  });
+
+  it('[Template] image SHOULD have exact image path WHEN initialize', () => {
+    // Arrange
+    const elem: HTMLImageElement = fixture.debugElement.query(By.css("[dataTestID=\"aside-image\"]")).nativeElement
+    // Assert
+    expect(elem.src).toContain(MOCKED_DATA.asideImageURL)
   });
 });
